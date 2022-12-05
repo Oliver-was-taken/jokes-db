1778f07a7e0bb9c6281aacf89457db663fbd6c33
Oliver <75665514+Oliver-was-taken@users.noreply.github.com>
2022-12-05 09:16:20 +0100

    Rating Tests

---
+++ src/test/java/ch/bbw/m151/jokesdb/controller/RatingsControllerTest.java
@@ -0,0 +1,31 @@
+package ch.bbw.m151.jokesdb.controller;
+
+import ch.bbw.m151.jokesdb.datamodel.JokesEntity;
+import ch.bbw.m151.jokesdb.repository.JokesRepository;
+import ch.bbw.m151.jokesdb.repository.RatingRepository;
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+class RatingsControllerTest implements WithAssertions {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Autowired
+    RatingRepository repository;
+
+    @Test
+    void rateAJoke() {
+        webTestClient.post().uri("/rateJoke?jokeId=1&starRating=1,political&description=KeinGuterWitz").exchange();
+        assertThat(repository.findAll()).isNotNull();
+    }
+}
